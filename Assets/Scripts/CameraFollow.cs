1fe59e9e4ef2228b959a92f348845d0fbe432960
aellis21 <andr3w3llis@gmail.com>
2019-07-08 17:46:54 +1000

    added missing files

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float xOffset = 0f;
+    public float zOffset = 0f;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x + xOffset, 
+            transform.position.y, player.position.z + zOffset);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float _moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        _moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, _moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
