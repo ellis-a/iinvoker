83372b8e10a94a7826c6506290e74c57dbb98e98
Edward Hummerston <ed.hummerston@gmail.com>
2019-07-13 16:53:37 +1000

    Invoke Element inputs

---
+++ Assets/Scripts/Elements.cs
@@ -0,0 +1,13 @@
+﻿public enum MagicElement
+{
+    Fire,
+    Earth,
+    Light,
+    Demon,
+    Storm,
+    Time,
+    Blood,
+    Illusion,
+    Hexes,
+    Life,
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     public float MovementSpeed;
+
+    public List<MagicElement> ElementLoadout;
+    public List<MagicElement> InvokedElements;
+    private bool isInvoke1Input = false;
+    private bool isInvoke2Input = false;
+    private bool isInvoke3Input = false;
+
+    public int MaxInvokedElements = 2;
+
     private Plane GroundPlane = new Plane(Vector3.up, 0);
     void Start()
     {
@@ -15,11 +25,12 @@ public class PlayerController : MonoBehaviour
         HandleMovementSpeed();
         HandleRotationInput();
         HandleShootInput();
+        HandleInvokeInputs();
     }
 
     void HandleMovementSpeed()
     {
-        transform.Translate(MovementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 
+        transform.Translate(MovementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f,
             MovementSpeed * Input.GetAxis("Vertical") * Time.deltaTime, Space.World);
     }
 
@@ -39,6 +50,37 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("Fire1"))
         {
             PlayerWand.Instance.Shoot();
+        }
+    }
+
+    void HandleInvokeInputs()
+    {
+        HandleSingleInvokeInput("Invoke1", 0, ref isInvoke1Input);
+        HandleSingleInvokeInput("Invoke2", 1, ref isInvoke2Input);
+        HandleSingleInvokeInput("Invoke3", 2, ref isInvoke3Input);
+    }
+
+    void HandleSingleInvokeInput(string buttonName, int loadoutIndex, ref bool isInputPressed)
+    {
+        if (Input.GetButton(buttonName))
+        {
+            if (!isInputPressed)
+            {
+                isInputPressed = true;
+                if (ElementLoadout.Count > loadoutIndex)
+                {
+                    InvokedElements.Insert(0, ElementLoadout[loadoutIndex]);
+
+                    if (InvokedElements.Count > MaxInvokedElements)
+                    {
+                        InvokedElements.RemoveAt(InvokedElements.Count - 1);
+                    }
+                }
+            }
+        }
+        else
+        {
+            isInputPressed = false;
         }
     }
 }
